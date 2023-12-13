15d52d1e960e804a4964c2af1e5facd76cd66aaf
TedMcinty <e.ailloud.mcintyre@gmail.com>
2023-12-13 12:19:41 +0100

    build cmake project

---
+++ build/CMakeFiles/nanobind-static.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/eaill/OneDrive/Documents/GitHub/cppgame/ext/nanobind/src/common.cpp" "CMakeFiles/nanobind-static.dir/ext/nanobind/src/common.cpp.o" "gcc" "CMakeFiles/nanobind-static.dir/ext/nanobind/src/common.cpp.o.d"
+  "/mnt/c/Users/eaill/OneDrive/Documents/GitHub/cppgame/ext/nanobind/src/error.cpp" "CMakeFiles/nanobind-static.dir/ext/nanobind/src/error.cpp.o" "gcc" "CMakeFiles/nanobind-static.dir/ext/nanobind/src/error.cpp.o.d"
+  "/mnt/c/Users/eaill/OneDrive/Documents/GitHub/cppgame/ext/nanobind/src/implicit.cpp" "CMakeFiles/nanobind-static.dir/ext/nanobind/src/implicit.cpp.o" "gcc" "CMakeFiles/nanobind-static.dir/ext/nanobind/src/implicit.cpp.o.d"
+  "/mnt/c/Users/eaill/OneDrive/Documents/GitHub/cppgame/ext/nanobind/src/nb_enum.cpp" "CMakeFiles/nanobind-static.dir/ext/nanobind/src/nb_enum.cpp.o" "gcc" "CMakeFiles/nanobind-static.dir/ext/nanobind/src/nb_enum.cpp.o.d"
+  "/mnt/c/Users/eaill/OneDrive/Documents/GitHub/cppgame/ext/nanobind/src/nb_func.cpp" "CMakeFiles/nanobind-static.dir/ext/nanobind/src/nb_func.cpp.o" "gcc" "CMakeFiles/nanobind-static.dir/ext/nanobind/src/nb_func.cpp.o.d"
+  "/mnt/c/Users/eaill/OneDrive/Documents/GitHub/cppgame/ext/nanobind/src/nb_internals.cpp" "CMakeFiles/nanobind-static.dir/ext/nanobind/src/nb_internals.cpp.o" "gcc" "CMakeFiles/nanobind-static.dir/ext/nanobind/src/nb_internals.cpp.o.d"
+  "/mnt/c/Users/eaill/OneDrive/Documents/GitHub/cppgame/ext/nanobind/src/nb_ndarray.cpp" "CMakeFiles/nanobind-static.dir/ext/nanobind/src/nb_ndarray.cpp.o" "gcc" "CMakeFiles/nanobind-static.dir/ext/nanobind/src/nb_ndarray.cpp.o.d"
+  "/mnt/c/Users/eaill/OneDrive/Documents/GitHub/cppgame/ext/nanobind/src/nb_static_property.cpp" "CMakeFiles/nanobind-static.dir/ext/nanobind/src/nb_static_property.cpp.o" "gcc" "CMakeFiles/nanobind-static.dir/ext/nanobind/src/nb_static_property.cpp.o.d"
+  "/mnt/c/Users/eaill/OneDrive/Documents/GitHub/cppgame/ext/nanobind/src/nb_type.cpp" "CMakeFiles/nanobind-static.dir/ext/nanobind/src/nb_type.cpp.o" "gcc" "CMakeFiles/nanobind-static.dir/ext/nanobind/src/nb_type.cpp.o.d"
+  "/mnt/c/Users/eaill/OneDrive/Documents/GitHub/cppgame/ext/nanobind/src/trampoline.cpp" "CMakeFiles/nanobind-static.dir/ext/nanobind/src/trampoline.cpp.o" "gcc" "CMakeFiles/nanobind-static.dir/ext/nanobind/src/trampoline.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
